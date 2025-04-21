b2a4892d830ad3f47f1ad803aca4eb37e3de26e1
fernando <fernandopaucar149@gmail.com>
2025-04-21 13:40:04 -0500

    chore(stego_rust): initialize project with configuration files and module structure

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+pub mod core {
+    pub mod encoder;
+    pub mod decoder;
+}
+
+pub mod crypto {
+    pub mod crypto;
+}
+
+pub mod utils {
+    pub mod index_db;
+}
+
+pub mod formats {
+    pub mod header;
+}
